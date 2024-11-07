ec14bf9891be61f0e65a979330de92b9581ec805
kendfss <kenneth@kendfss.xyz>
2024-11-07 22:59:41 +0100

    add: binary operation wrappers

--- math/math.go
+++ math/math.go
@@ -2,6 +2,7 @@ package math
 
 import "github.com/kendfss/rules"
 
+<<<<<<< HEAD
 // Add returns the sum of the given argumens
 func Add[T rules.Num](a, b T) T {
 	return a + b
@@ -21,3 +22,24 @@ func Sub[T rules.Num](a, b T) T {
 func Div[T rules.Num](a, b T) T {
 	return a / b
 }
+=======
+// // Add returns the sum of the given argumens
+// func Add[T rules.Num](a, b T) T {
+// 	return a + b
+// }
+
+// // Mul multiplies the first argument by the second argument
+// func Mul[T rules.Num](a, b T) T {
+// 	return a * b
+// }
+
+// // Sub subtracts the first argument by the second argument
+// func Sub[T rules.Num](a, b T) T {
+// 	return a - b
+// }
+
+// // Div divides the first argument by the second argument
+// func Div[T rules.Num](a, b T) T {
+// 	return a / b
+// }
+>>>>>>> 4c0ffb7 (add: binary operation wrappers)
